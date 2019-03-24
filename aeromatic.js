8acd09fadc0f9ec13c1fc1dc687ec8a2c2c0a965
Bertrand Coconnier <bcoconni@users.sf.net>
2019-03-24 01:34:39 +0100

    Completed the piston engine.

--- aeromatic.js
+++ aeromatic.js
@@ -6,9 +6,9 @@ $(document).ready(
 
         $('input#engine_generate').click(function() {
             var name = $('#ac_enginename').val();
-            var type = $('input[name=ac_enginetype]:checked').val();
+            var type = Number($('input[name=ac_enginetype]:checked').val());
             var power = Number($('#ac_enginepower').val());
-            var units = $('input[name=ac_engineunits]:checked').val();
+            var units = Number($('input[name=ac_engineunits]:checked').val());
             var augmented = $('#ac_augmented').prop('checked');
             var injected = $('#ac_injected').prop('checked');
 
@@ -21,21 +21,23 @@ $(document).ready(
                 power *= 0.2248;
 
             var comments = '<--\n  File:     '+name+'.xml\n  Author:   Aero-Matic v '+version+'\n\n  Inputs\n    name:           '+name+'\n    type:           ';
+            var engine;
 
-            if (type == 0) {
-                comments += 'piston'
-                var engine = $('<piston_engine/>');
-                var minmp = $('<minmp>');
-                minmp.attr('unit', 'INHG');
-                minmp.text('10.0');
-                engine.append(minmp);
+            switch(type) {
+                case 0:
+                    comments += 'piston'
+                    engine = MakePiston(power);
+                    break;
+                case 1:
+                    engine = $('<turbine_engine/>');
+                    break;
+                case 2:
+                    engine = $('<turbine_engine/>');
+                    break;
+                case 3:
+                    engine = $('<rocket_engine/>');
+                    break;
             }
-            else if (type == 1)
-                var engine = $('<turbine_engine/>');
-            else if (type == 2)
-                var engine = $('<turbine_engine/>');
-            else if (type == 3)
-                var engine = $('<rocket_engine/>');
 
             engine.attr('name', name);
 
@@ -44,11 +46,11 @@ $(document).ready(
             switch(units) {
                 case 0:
                 case 2:
-                    comments += 'hp';
+                    comments += ' hp';
                     break;
                 case 1:
                 case 3:
-                    comments += 'lb';
+                    comments += ' lb';
                     break;
             }
 
@@ -78,6 +80,52 @@ $(document).ready(
             var link = $('#download-link');
             link.attr('href', url);
             link.attr('download', 'engine2.php');
-            document.getElementById('download-link').click();
+            //document.getElementById('download-link').click();
         });
     });
+
+function MakePiston(power) {
+    var engine = $('<piston_engine/>');
+    add_tag(engine, 'minmp', '10.0', 'INHG');
+    add_tag(engine, 'maxmp', '28.5', 'INHG');
+
+    var displ = power * 1.9;
+    add_tag(engine, 'displacement', displ.toFixed(2), 'IN3');
+    add_tag(engine, 'maxhp', power.toFixed(2));
+     add_tag(engine, 'cycles', 4);
+    add_tag(engine, 'idlerpm', 700);
+    add_tag(engine, 'maxrpm', 2800);
+    add_tag(engine, 'sparkfaildrop', 0.1);
+    add_tag(engine, 'volumetric-efficiency', 0.85);
+    add_tag(engine, 'man-press-lag', 0.1);
+    add_tag(engine, 'static-friction', (power*0.005).toFixed(2), 'HP');
+    add_tag(engine, 'starter-torque', (power*0.8).toFixed(2));
+    add_tag(engine, 'starter-rpm', 1400);
+
+    engine.append('<!-- Defining <bsfc> over-rides the built-in horsepower calculations -->');
+    engine.append('<!--<bsfc>           0.45 </bsfc>-->');
+
+    var stroke = 4.375; // FIXME: we can use maxrpm to find a stroke length
+    var bore = 5.125;
+    var bore_s = Math.pow(bore/2, 2.0) * 3.14159; // Guess the area of one piston (5.125/2)^2 * PI
+    var n_cylinders = displ / (stroke * bore_s);
+    if (n_cylinders < 1)
+        n_cylinders = 1;
+    else
+        n_cylinders = Math.floor(n_cylinders+0.5);
+
+    add_tag(engine, 'stroke', stroke.toFixed(3), 'IN');
+    add_tag(engine, 'bore',bore.toFixed(3), 'IN');
+    add_tag(engine, 'cylinders', n_cylinders.toFixed(1));
+    add_tag(engine, 'compression-ratio', 8.0);
+
+    return engine;
+}
+
+function add_tag(engine, name, value, unit=null) {
+    var tag = $('<'+name+'>');
+    if (unit)
+        tag.attr('unit', unit);
+    tag.text(value);
+    engine.append(tag);
+}
